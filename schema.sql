022bdda427a23cf51165034420d89d9f65de876e
Yakini <yakinilatha09@gmail.com>
2020-08-10 14:44:58 -0600

    Added schema.sql

---
+++ schema.sql
@@ -0,0 +1,29 @@
+DROP DATABASE IF EXISTS employeeTracker_DB;
+CREATE DATABASE employeeTracker_DB;
+
+USE employeeTracker_DB;
+
+
+CREATE TABLE department(
+   id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+   name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role(
+   id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+   title VARCHAR(30) NOT NULL,
+   salary DECIMAL(10,2) NOT NULL,
+   department_id INT UNSIGNED NOT NULL,
+   CONSTRAINT FK_departmentId FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+CREATE TABLE employee(
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT UNSIGNED NOT NULL,
+  CONSTRAINT FK_roleId FOREIGN KEY (role_id) REFERENCES role(id),
+  manager_id INT UNSIGNED NOT NULL,
+  CONSTRAINT FK_managerId FOREIGN KEY (manager_id) REFERENCES employee(id)
+ 
+);
