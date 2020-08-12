a52e2ef49f9957000ac0a0536d8585dcff8b2527
Yakini <yakinilatha09@gmail.com>
2020-08-12 00:31:41 -0600

    Add Employee- Working

--- employeeTracker.js
+++ employeeTracker.js
@@ -1,135 +1,135 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+const util = require("util");
 var consoleTable = require("console.table")
 
 let connection;
 connection = mysql.createConnection({
-    host: "localhost",
-  
-    // Your port; if not 3306
-    port: 3306,
-  
-    // Your username
-    user: "root",
-  
-    // Your password
-    password: "webDev@2020",
-    database: "employeeTracker_DB"
-  });
-  
+  host: "localhost",
 
-  function inquirerPrompts(){
-    inquirer.prompt([
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "",
+  database: "employeeTracker_DB"
+});
+
+
+
+function inquirerPrompts() {
+  inquirer.prompt([
     {
-        type: "list",
-        message: "Which you like to do?",
-        name: "choice",
-        choices: [
-          "View All Employees",
-          "View All Employees by Department",
-          "View All Employees by Manager",
-          "Add Employee",
-          "Remove Employee",
-          "Update Employee Role",
-          "Update Employee Manager"
-        ]
-        }]).then(teamChoice => {
+      type: "list",
+      message: "Which you like to do?",
+      name: "choice",
+      choices: [
+        "View All Employees",
+        "View All Employees by Department",
+        "View All Employees by Manager",
+        "Add Employee",
+        "Remove Employee",
+        "Update Employee Role",
+        "Update Employee Manager"
+      ]
+    }]).then(teamChoice => {
 
-        switch(teamChoice.choice){
+      switch (teamChoice.choice) {
 
         case `View All Employees`:
-            viewAllEmployees();
-            
-            break;
-        
+          viewAllEmployees();
+
+          break;
+
         case `View All Employees by Department`:
-        
-            viewEmployeesByDept();
-            
-            break;
+
+          viewEmployeesByDept();
+
+          break;
 
         case `View All Employees by Manager`:
-        
-            viewEmployeesByManager();
-            
-            break;
+
+          viewEmployeesByManager();
+
+          break;
 
         case `Add Employee`:
-        
-            addEmployee();
-            
-            break;
+
+          addEmployee();
+
+          break;
 
         case `RemoveEmployee`:
-    
-            removeEmployee();
-            
-            break;
+
+          removeEmployee();
+
+          break;
 
         case `Update Employee Role`:
 
-            updateEmployeeRole();
-            
-            break;
+          updateEmployeeRole();
+
+          break;
 
         case `Update Employee Manager`:
 
-            updateEmployeeManager();
-            
-            break;
+          updateEmployeeManager();
 
-         default:
-          
-            break;
+          break;
 
-          }
-        
-        })
-        .catch(function(err) {
-        console.log(err);
-      });
+        default:
+
+          break;
+
+      }
+
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+}
+
+function viewAllEmployees() {
+  console.log("View All Employees...\n");
+
+  var query = "SELECT e.id , e.first_name, e.last_name, r.title,  d.name as department, r.salary, CONCAT(m.first_name,' ',m.last_name) as manager from employee e ";
+  query += "LEFT JOIN role r ON e.role_id = r.id ";
+  query += "LEFT JOIN department d ON r.department_id = d.id ";
+  query += "LEFT JOIN employee m ON m.id = e.manager_id"
+
+  connection.query(query, function (err, res) {
+    console.log(res.length + " matches found!");
+
+    console.table(res);
+    inquirerPrompts();
+  });
+
+}
+
+  function addEmployee() {
+  //  const role =  returnRole();
+  //  const manager =  returnManager();
+  var rolechoiceArray = [];
+  var managerchoiceArray = [];
+
+  connection.query("SELECT * from role", function (err, role) {
+
+    for (var i = 0; i < role.length; i++) {
+      rolechoiceArray.push(role[i].title);
+
     }
- 
-    function viewAllEmployees() {
-        console.log("View All Employees...\n");
-   
-        var query = "SELECT e.id , e.first_name, e.last_name, r.title,  d.name as department, r.salary, CONCAT(m.first_name,' ',m.last_name) as manager from employee e " ;
-          query += "LEFT JOIN role r ON e.role_id = r.id ";
-          query += "LEFT JOIN department d ON r.department_id = d.id " ;
-          query += "LEFT JOIN employee m ON m.id = e.manager_id"
-        
-          connection.query(query, function(err, res) {
-            console.log(res.length + " matches found!");
-        
-            console.table(res);
-            inquirerPrompts();
-         });
-       
-       }
 
-    function addEmployee() {
-        //  const role =  returnRole();
-        //  const manager =  returnManager();
-         var rolechoiceArray = [];
-         var managerchoiceArray = [];
+    connection.query("SELECT  CONCAT(employee.first_name,' ',employee.last_name) as managerName from employee", function (err, manager) {
 
-         connection.query("SELECT * from role", function(err, role) {
-         
-          for (var i = 0; i < role.length; i++) {
-            rolechoiceArray.push(role[i].title);
-           
-          }
+      for (var i = 0; i < manager.length; i++) {
+        managerchoiceArray.push(manager[i].managerName);
 
-      
+      }
 
-         connection.query("SELECT  CONCAT(employee.first_name,' ',employee.last_name) as managerName from employee", function(err, manager) {
-         
-          for (var i = 0; i < manager.length; i++) {
-            managerchoiceArray.push(manager[i].managerName);
-           
-          }
-
-        inquirer
+       inquirer
         .prompt([
           {
             name: "firstName",
@@ -142,10 +142,10 @@ connection = mysql.createConnection({
             message: "What is the employee's last Name?"
           },
           {
-          name: "role",
-          type: "rawlist",
-          choices: rolechoiceArray
-          
+            name: "role",
+            type: "rawlist",
+            choices: rolechoiceArray
+
           },
 
           {
@@ -153,70 +153,73 @@ connection = mysql.createConnection({
             type: "rawlist",
             choices: managerchoiceArray
           }
-        ])
-    .then(function(answer) {
+        ]).then(async function(answer) {
+
       // when finished prompting, insert a new item into the db with that info
-        var deptId = deptID(answer.role);
-        var managerId = managerID(answer.managerName);
-        console.log(deptId);
-        console.log(managerId);
-
-        "INSERT INTO employee SET ?",
-        {
-          first_name: answer.firstName,
-          last_name: answer.lastName,
-          role_id: deptId,
-          manager_id: managerId
-        },
-
-        function(err) {
+ 
+       connection.query("SELECT * from role where role.title = '" +answer.role +"'", function(err, resdeptId) {
+         var deptId;
           if (err) throw err;
+
+          resdeptId.find(depId => {
+          deptId = depId.department_id
+          }
+        );
+          console.log(deptId);
+          
+          var managerName = answer.managerName;
+          var firstName = managerName.split(" ",1)+"%";
+       connection.query("SELECT * from employee where employee.first_name LIKE '" +firstName+"'", function(err , resmanagerId){
+          if (err) throw err;
+         var managerId;
+         resmanagerId.find(mgrId =>{
+          managerId = mgrId.id
+         })
+      console.log(managerId);
+      
+      var query = connection.query("INSERT INTO employee SET ?",
+      {
+        first_name: answer.firstName,
+        last_name: answer.lastName,
+        role_id: deptId,
+        manager_id: managerId
+      },
+
+   
+        function (err) {
+          if (err) throw err;
+        
           console.log("Your employee data got inserted successfully!");
           // re-prompt the user for if they want to bid or post
           inquirerPrompts();
         }
+      );
+    // );
+     });
     });
     });
+});
+});
+}
+
+ function deptID(deptName) {
+
+
+  var result= "";
+  connection.query("SELECT department_id from role where role.title = '" +deptName +"'", function(err, res) {
+    if (err) throw err;
+    result = res;
+    console.log(result);
   });
+  return result;
+
 }
 
 
-    //  function returnRole(){
-    
-    //   connection.query("SELECT * from role", function(err, role) {
-    //      return role;
-    //    })
-    //    }
- 
-    //    function returnManager(){
-    //     connection.query("SELECT  CONCAT(employee.first_name,' ',employee.last_name) as manager from employee", function(err , manager) { 
-    //       return manager;
-    //    })
-
-    //    }
-
-
-       function deptID(deptName){
-         var deptId = "";
-        var query = "SELECT department_id from role where role.title = ?"
-        connection.query(query, deptName, function(err, resdeptId) {
-          
-          deptId = resdeptId;
-           
-        });
-        return deptId;
-       
-      }
-
-
-      function managerID(managerName){
-        var managerId = "";
-        var query = "SELECT employee.id from employee where CONCAT(employee.first_name,' ',employee.last_name) = ?"
-        connection.query(query, managerName , function(err , resManager){
-       
-          managerId = resManager;
-        
-        })
-        return managerId;
-      }
-       inquirerPrompts();
+ function managerID(managerName) {
+  // connection.query = util.promisify(connection.query);
+  var managerId = "";
+  var query = "SELECT employee.id from employee where CONCAT(employee.first_name,' ',employee.last_name) = ?"
+  return  connection.query(query, managerName)
+}
+inquirerPrompts();
